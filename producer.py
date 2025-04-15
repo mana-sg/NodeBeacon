9e6c18398bb71fdcc508113b5f3479d68f939a52
Aneesh200 <aneeshmadhav2004@gmail.com>
2025-04-15 13:43:57 +0530

    First Iteration

--- producer.py
+++ producer.py
@@ -9,7 +9,7 @@ producer = KafkaProducer(bootstrap_servers='localhost:9092')
 topic = 'video-stream'
 
 # Camera setup
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_FPS, 30)  # Try increasing this if camera supports it
